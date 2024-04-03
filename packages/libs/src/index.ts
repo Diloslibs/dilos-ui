edec362aac9e583e1994afbeaa8e4f04edf7c805
adzibilal <adzibilal02@gmail.com>
2024-04-03 12:29:51 +0700

    feat(dropdown): add dropdown component and variants

--- packages/libs/src/index.ts
+++ packages/libs/src/index.ts
@@ -1,5 +1,7 @@
 import Accordion from './components/accordion';
+import Dropdown from './components/dropdown';
 import Table from './components/table';
 
 export { Accordion };
+export { Dropdown };
 export { Table };
