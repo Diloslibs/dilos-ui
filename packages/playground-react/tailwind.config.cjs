3d197b64acacb65215385da1bc0d55bce91a0107
Nur Sasongko <kokonur1020@gmail.com>
2024-05-07 21:34:40 +0700

    chore(playground-react): Update page title in index.html

--- packages/playground-react/tailwind.config.cjs
+++ packages/playground-react/tailwind.config.cjs
@@ -3,13 +3,10 @@ export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./node_modules/@dilos/libs/dist/*.js",
-    // 'node_modules/libs/dist/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    // require('libs/plugin'),
-  ],
+  plugins: [],
 }
 
