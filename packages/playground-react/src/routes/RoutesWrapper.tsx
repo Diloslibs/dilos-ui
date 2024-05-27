fd41693f8e67b1ddb710c07d6f2cd544a59deac6
Nur Sasongko <kokonur1020@gmail.com>
2024-05-27 14:03:59 +0700

    feat: Add Breadcrumb component and related styles

--- packages/playground-react/src/routes/RoutesWrapper.tsx
+++ packages/playground-react/src/routes/RoutesWrapper.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "@/layouts/MainLayout";
 import HomePage from "@/pages/HomePage";
-import Accordion from "@/components/Accordion";
 import Dropdown from "@/components/Dropdown";
 import Tooltip from "@/components/Tooltip";
+import AccordionPage from "@/pages/Containment/AccordionPage";
 
 const RouteWrapper: React.FC = () => {
   return (
@@ -11,7 +11,7 @@ const RouteWrapper: React.FC = () => {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
-          <Route path="/accordion" element={<Accordion />} />
+          <Route path="/accordion" element={<AccordionPage />} />
           <Route path="/dropdown" element={<Dropdown />} />
           <Route path="/tooltip" element={<Tooltip />} />
         </Route>
